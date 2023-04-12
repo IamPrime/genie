908c80b860740ecee57406fd8825055e4a72219a
Tanuja <kollt01@pfw.edu>
2023-04-12 12:21:15 +0530

    added notification component, fixed dropdown and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import DropDown from "./DropDown";
+import Notification from "./Notification";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../utils/firebase";
 import { FaBell } from "react-icons/fa";
 
 function Navbar() {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
+    setShowNotification(false);
+  };
+  const toggleNotification = () => {
+    setShowNotification(!showNotification);
+    setShowDropdown(false);
   };
   const [user, loading] = useAuthState(auth);
-
-  const handleClick = () => {
-    setShowDropdown(!showDropdown);
-  };
 
   return (
     <>
@@ -23,7 +26,7 @@ function Navbar() {
         <div className="mx-50 my-2 px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between">
             <div className="absolute inset-y-0 left-0 items-center hidden">
-              {/* Mobile menu button*/}
+              {/** Mobile menu button*/}
               <button
                 type="button"
                 className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -31,7 +34,7 @@ function Navbar() {
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
-                {/Icon when menu is closed.Menu open: "hidden", Menu closed: "block"/}
+                {/**Icon when menu is closed.Menu open: "hidden", Menu closed: "block"*/}
                 <svg
                   className="block h-6 w-6"
                   fill="none"
@@ -46,7 +49,7 @@ function Navbar() {
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
-                {/Icon when menu is open. Menu open: "block", Menu closed: "hidden"/}
+                {/**Icon when menu is open. Menu open: "block", Menu closed: "hidden"*/}
                 <svg
                   className="hidden h-6 w-6"
                   fill="none"
@@ -73,7 +76,7 @@ function Navbar() {
               </div>
               <div className="hidden sm:ml-20 sm:block right-20 w-full">
                 <div className="flex items-center justify-between space-x-20 font-mono">
-                  {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                  {/** Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                   <Link
                     href={"/"}
                     className="hover:bg-pink-700 text-black hover:text-white rounded-md px-3 py-2 text-m font-medium"
@@ -99,8 +102,23 @@ function Navbar() {
                   >
                     Categories
                   </Link>
-
                   <div className="space-x-5 inset-y-0 right-0 flex flex-end pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 font-mono">
+                    <button
+                      onClick={toggleNotification}
+                      className="text-black hover:text-pink-700 block rounded-md px-3 py-2 text-base font-medium"
+                    >
+                      {user && (
+                        <div className="top-0 right-70 left-80">
+                          <FaBell className="w-6 h-4 text-pink-700" />
+                          <span className="animate-ping absolute top-6 right-24 text-lg block rounded-full ring-2 ring-black bg-black"></span>
+                          {showNotification && (
+                            <div className="absolute top-full mt-2 right-0 z-10">
+                              <Notification />
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </button>
                     <button
                       onClick={toggleDropdown}
                       className="text-black hover:text-pink-700 block rounded-md px-3 py-2 text-base font-medium"
@@ -117,30 +135,19 @@ function Navbar() {
                       )}
                       {user && (
                         <div className="flex items-center justify-between space-x-6">
-                          <div className="top-0 right-70 left-80">
-                            <FaBell className="w-8 h-4 text-red-500" />
-                            <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full px-1"></span>
+                          <div>
+                            <img
+                              referrerPolicy="no-referrer"
+                              className="w-8 rounded-full"
+                              src={user.photoURL}
+                              alt="Avatar"
+                            />
+                            {showDropdown && (
+                              <div className="absolute top-full mt-2 right-0 z-10">
+                                <DropDown />
+                              </div>
+                            )}
                           </div>
-                          <button
-                            onClick={handleClick}
-                            className="focus:outline-none"
-                          >
-                            <div>
-                              
-                                <img
-                                  referrerPolicy="no-referrer"
-                                  className="w-8 rounded-full"
-                                  src={user.photoURL}
-                                  alt="Avatar"
-                                />
-                              
-                              {showDropdown && (
-                                <div className="absolute top-full mt-2 right-0 z-10">
-                                  <DropDown />
-                                </div>
-                              )}
-                            </div>
-                          </button>
                         </div>
                       )}
                     </button>
@@ -181,51 +188,54 @@ function Navbar() {
               Categories
             </Link>
             <div className="space-x-5 inset-y-0 right-0 flex flex-end pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 font-mono">
-              <div className="relative">
-                <button
-                  onClick={toggleDropdown}
-                  className="text-black hover:text-pink-700 block rounded-md px-3 py-2 text-base font-medium"
-                >
-                  {!user && (
+              <button
+                onClick={toggleNotification}
+                className="text-black hover:text-pink-700 block rounded-md px-3 py-2 text-base font-medium"
+              >
+                {user && (
+                  <div className="top-0 right-70 left-80">
+                    <FaBell className="w-6 h-4 text-pink-700" />
+                    <span className="animate-ping absolute top-6 right-24 text-lg block rounded-full ring-2 ring-black bg-black"></span>
+                    {showNotification && (
+                      <div className="absolute top-full mt-2 right-0 z-10">
+                        <Notification />
+                      </div>
+                    )}
+                  </div>
+                )}
+              </button>
+              <button
+                onClick={toggleDropdown}
+                className="text-black hover:text-pink-700 block rounded-md px-3 py-2 text-base font-medium"
+              >
+                {!user && (
+                  <div>
+                    <Link
+                      href={"/auth/Login"}
+                      className="bg-pink-700 text-white block rounded-md px-3 py-1 text-m font-medium"
+                    >
+                      Signup
+                    </Link>
+                  </div>
+                )}
+                {user && (
+                  <div className="flex items-center justify-between space-x-6">
                     <div>
-                      <Link
-                        href={"/auth/Login"}
-                        className="bg-pink-700 text-white block rounded-md px-3 py-1 text-m font-medium"
-                      >
-                        Signup
-                      </Link>
-                    </div>
-                  )}
-                  {user && (
-                    <div>
-                      <button
-                        onClick={handleClick}
-                        className="focus:outline-none"
-                      >
-                        <div>
-                         
-                            <img
-                              referrerPolicy="no-referrer"
-                              className="w-8 rounded-full"
-                              src={user.photoURL}
-                              alt="Avatar"
-                            />
-                         
-                          {showDropdown && (
-                            <div className="absolute top-full mt-2 right-0 z-10">
-                              <DropDown />
-                            </div>
-                          )}
+                      <img
+                        referrerPolicy="no-referrer"
+                        className="w-8 rounded-full"
+                        src={user.photoURL}
+                        alt="Avatar"
+                      />
+                      {showDropdown && (
+                        <div className="absolute top-full mt-2 right-0 z-10">
+                          <DropDown />
                         </div>
-                        <div className="absolute top-0 left-8">
-                          <FaBell className="w-4 h-4 text-red-500" />
-                          <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full px-1"></span>
-                        </div>
-                      </button>
+                      )}
                     </div>
-                  )}
-                </button>
-              </div>
+                  </div>
+                )}
+              </button>
             </div>
           </div>
         </div>
