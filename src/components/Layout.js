82c29e757df2840da387877fd9bf63210f7add30
Tanuja <kollt01@pfw.edu>
2023-04-20 22:43:06 +0530

    added code fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({children}){
     return (
         <div>
             <Navbar />
-            <main className="mt-32 mb-32">{children}</main>
+            <main className="mb-32">{children}</main>
             <Footer/>
         </div>
     );
--- src/pages/private/quiz/HowTo.tsx
+++ src/pages/private/quiz/HowTo.tsx
@@ -1,71 +1,89 @@
+import { auth } from "../../../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
 
 export default function HowTo() {
-  return (
-    <>
-      <Head>
-        <title>Genie | Quiz Instructions</title>
-      </Head>
-      <section className="bg-gradient-to-r from-pink-500 to-blue-500 leading-relaxed p-10 text-amber-300 rounded-tl-3xl rounded-br-3xl mx-40 my-10">
-        <div className="grid justify-center items-center ">
-          <article className="prose md:prose-lg lg:prose-xl dark:prose-invert">
-            <h1>How to play with the Genie</h1>
-            <h3>Ensure to read the instructions before you start</h3>
-            <div>
-              <ul className="list-outside">
-                <li>
-                  <strong>Game Duration: &nbsp;</strong>15 total minutes is
-                  allowed for each quiz type.
-                </li>
-                <li>
-                  <strong>Questions Available: &nbsp;</strong> 15 total
-                  questions for each quiz type.
-                </li>
-                <li>
-                  <strong>Answer Options: &nbsp;</strong>Every question contains
-                  4 options.
-                  {/** We can add a screenshot here */}
-                </li>
-                <li>
-                  <strong>Your Answer: &nbsp;</strong>Select the options you
-                  think best answers the question.
-                  {/** We can add a screenshot here */}
-                </li>
-                <li>
-                  <strong>Lifeline: &nbsp;</strong>Each quiz has 2 lifelines
-                  namely:
-                  <ul className="list-inside">
-                    <li>
-                      <strong>Toss-Up: &nbsp;</strong> Two (2)
-                    </li>
-                    <li>
-                      <strong>Hints: &nbsp;</strong> Five (5)
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  You are free to quit (retire) from the quiz at any time.
-                  <ul className="list-inside">
-                    <li>
-                      Your score will be revealed and the quiz will be over.
-                    </li>
-                  </ul>
-                </li>
-                <li>Timer: Starts as soon as the game loads.</li>
-              </ul>
-            </div>
-          </article>
-        </div>
-        <div className="flex justify-between items-center">
-          <span>
-            <Link className="text-white font-bold" href={"/private/Dashboard"}>Take me back</Link>
-          </span>
-          <span>
-            <Link className="text-white font-bold" href={"/private/quiz/Play"}>Continue</Link>
-          </span>
-        </div>
-      </section>
-    </>
-  );
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+  if (!user) route.push("/");
+  if (user) {
+    return (
+      <>
+        <Head>
+          <title>Genie | Quiz Instructions</title>
+        </Head>
+        <section className="bg-gradient-to-r from-pink-500 to-purple-500 leading-relaxed p-10 text-amber-300 rounded-tl-3xl rounded-br-3xl mx-40 my-10">
+          <div className="grid justify-center items-center ">
+            <article className="prose md:prose-lg lg:prose-xl dark:prose-invert">
+              <h1>How to play with the Genie</h1>
+              <h3>Ensure to read the instructions before you start</h3>
+              <div>
+                <ul className="list-outside">
+                  <li>
+                    <strong>Game Duration: &nbsp;</strong>15 total minutes is
+                    allowed for each quiz type.
+                  </li>
+                  <li>
+                    <strong>Questions Available: &nbsp;</strong> 15 total
+                    questions for each quiz type.
+                  </li>
+                  <li>
+                    <strong>Answer Options: &nbsp;</strong>Every question
+                    contains 4 options.
+                    {/** We can add a screenshot here */}
+                  </li>
+                  <li>
+                    <strong>Your Answer: &nbsp;</strong>Select the options you
+                    think best answers the question.
+                    {/** We can add a screenshot here */}
+                  </li>
+                  <li>
+                    <strong>Lifeline: &nbsp;</strong>Each quiz has 2 lifelines
+                    namely:
+                    <ul className="list-inside">
+                      <li>
+                        <strong>Toss-Up: &nbsp;</strong> Two (2)
+                      </li>
+                      <li>
+                        <strong>Hints: &nbsp;</strong> Five (5)
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    You are free to quit (retire) from the quiz at any time.
+                    <ul className="list-inside">
+                      <li>
+                        Your score will be revealed and the quiz will be over.
+                      </li>
+                    </ul>
+                  </li>
+                  <li>Timer: Starts as soon as the game loads.</li>
+                </ul>
+              </div>
+            </article>
+          </div>
+          <div className="flex justify-between items-center">
+            <span>
+              <Link
+                className="text-white font-bold"
+                href={"/private/Dashboard"}
+              >
+                Take me back
+              </Link>
+            </span>
+            <span>
+              <Link
+                className="text-white font-bold"
+                href={"/private/quiz/Play"}
+              >
+                Continue
+              </Link>
+            </span>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
